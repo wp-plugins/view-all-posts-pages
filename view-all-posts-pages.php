ff353ef96786fad4a145c58fec651f20b9b6c43e
ethitter <ethitter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-05 02:21:43 +0000

    Add license declaration.
    git-svn-id: https://plugins.svn.wordpress.org/view-all-posts-pages/trunk@553272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view-all-posts-pages.php
+++ view-all-posts-pages.php
@@ -6,6 +6,20 @@ Description: Provides a "view all" (single page) option for posts, pages, and cu
 Author: Erick Hitter (Oomph, Inc.)
 Version: 0.6.1
 Author URI: http://www.thinkoomph.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class view_all_posts_pages {
